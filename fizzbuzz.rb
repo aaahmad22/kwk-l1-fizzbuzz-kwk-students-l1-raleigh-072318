6bd5ba904b5358030406788316407428fd3d4b8f
aaahmad22 <aminahakeelahmad@gmail.com>
2018-07-25 00:08:48 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,12 @@
 def fizzbuzzuzz(number)
 if number % 3 == 0 && number % 5 == 0 
-   return "FizzBuzz"
+  puts "FizzBuzz"
 elsif number % 3 == 0
-  return "Fizz"
+  puts "Fizz"
 elsif number % 5 == 0
-  return "Buzz"
+  puts "Buzz"
 else
-  return nil
+  puts nil
  end
 end
 puts fizzbuzz(30)
